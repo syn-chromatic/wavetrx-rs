cfc6bed5bfd2fa4128fc6b4c8d9e2b0bfcc26b4c
syn-chromatic <synchromatic.github@gmail.com>
2024-02-10 05:36:06 +0200

    update consts

--- wavetrx/src/consts.rs
+++ wavetrx/src/consts.rs
@@ -10,15 +10,15 @@ pub const LP_FILTER: f32 = 18_000.0;
 pub const HP_FILTER: f32 = 200.0;
 
 // Protocol Profile
-pub const MARKER_TONE_START: f32 = 13_000.0;
-pub const MARKER_TONE_END: f32 = 16_000.0;
-pub const MARKER_TONE_NEXT: f32 = 5_000.0;
+pub const MARKER_TONE_START: f32 = 7_000.0;
+pub const MARKER_TONE_END: f32 = 9_000.0;
+pub const MARKER_TONE_NEXT: f32 = 3_000.0;
 
-pub const BIT_TONE_HIGH: f32 = 9_000.0;
+pub const BIT_TONE_HIGH: f32 = 5_000.0;
 pub const BIT_TONE_LOW: f32 = 1_000.0;
 
-pub const PULSE_LENGTH_US: Duration = Duration::from_micros(500);
-pub const PULSE_GAP_US: Duration = Duration::from_micros(1000);
+pub const PULSE_LENGTH_US: Duration = Duration::from_micros(1000);
+pub const PULSE_GAP_US: Duration = Duration::from_micros(2000);
 
 // DETECTION THRESHOLD
 pub const DB_THRESHOLD: f32 = 8.0;
